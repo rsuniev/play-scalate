339f9227212bd8dfcaee87c85f3a2437a15151cd
peter hausel <peter_kovac11@hotmail.com>
2010-02-23 08:37:22 -0500

    use trait instead of object

--- src/play/mvc/ScalateController.scala
+++ src/play/mvc/ScalateController.scala
@@ -1,9 +1,9 @@
 package play.mvc
 
  
-abstract class ScalateController extends play.mvc.ScalaController {
+abstract class ScalateController extends play.mvc.ScalaController with ScalateProvider {
   def renderScalate(args: Any*) {
-    ScalateProvider.renderOrProvideTemplate(args.map(_.asInstanceOf[AnyRef])) match {
+    renderOrProvideTemplate(args.map(_.asInstanceOf[AnyRef])) match {
       case Some(template) => renderTemplate(template,args.map(_.asInstanceOf[AnyRef]))
       case None => 
     }
